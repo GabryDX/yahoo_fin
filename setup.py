823485f4d1abb01b7fbb7ea29300a998615b6d04
GabryDX <gabrydx@hotmail.com>
2023-04-04 12:55:52 +0200

    Fix no income statements availible with exception handling

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(name='yahoo_fin',
-      version='0.8.9.1',
+      version='0.8.9.2',
       description="""Download historical stock prices (daily / weekly / monthly),
                      realtime-prices, fundamentals data, income statements, 
                      cash flows, analyst info, current cryptocurrency prices,
                      option chains, earnings history, and more with yahoo_fin.
                     """,
       url='http://theautomatic.net/yahoo_fin-documentation/',
-      author='Andrew Treadway',
+      author='Andrew Treadway, Gabriele Nicosanti',
       author_email='opensourcecoder11@gmail.com',
       license='MIT',
       packages=['yahoo_fin'],
