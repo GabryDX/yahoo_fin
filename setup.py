35800b2f4f6b87362a932c1394fcb6caf07fa3f8
GabryDX <gabrydx@hotmail.com>
2023-04-04 16:17:32 +0200

    Added method for decryption of encrypted blob coming back from yahoo finance in _parse_json() and add get_live_prices() as a rapid method to retrieve prices for a list of ticker symbols with a single request to finance.yahoo

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ setup(name='yahoo_fin',
                      realtime-prices, fundamentals data, income statements, 
                      cash flows, analyst info, current cryptocurrency prices,
                      option chains, earnings history, and more with yahoo_fin.
-                    """,
+                  """,
       url='http://theautomatic.net/yahoo_fin-documentation/',
       author='Andrew Treadway, Gabriele Nicosanti',
       author_email='opensourcecoder11@gmail.com',
       license='MIT',
       packages=['yahoo_fin'],
-      install_requires = ["requests_html", "feedparser", "requests", "pandas"],
-      keywords = ["yahoo finance", "stocks", "options", "fundamentals"],
-zip_safe=False)
+      install_requires=["requests_html", "feedparser", "requests", "pandas", "numpy", "pycryptodome"],
+      keywords=["yahoo finance", "stocks", "options", "fundamentals"],
+      zip_safe=False)
